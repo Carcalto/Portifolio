2ea0d0a57a14124a6d682e03b82eb3e6f1a6af20
Carcalto <carcalto@bol.com.br>
2022-12-29 01:54:16 -0400

    Up

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-/* menu animado em JS */
-
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-});
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
 
